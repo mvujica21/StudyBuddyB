e51be47386f468abc13665ada7f5287597810892
Mateo Vujica <94994876+mvujica21@users.noreply.github.com>
2025-02-24 15:42:01 +0100

    Implement JWT authentication with login functionality and protected routes using Passports. To add JWT guard just use "@UseGuards(JwtAuthGuard)" #1 #4 #6

--- study-buddy-b/src/auth/auth.module.ts
+++ study-buddy-b/src/auth/auth.module.ts
@@ -3,10 +3,25 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/User';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: '1h' },
+      global: true,
+    }),
+    inject: [ConfigService],
+  }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService]
 })
 export class AuthModule { }
